a42a740c3f340749fed155e6332478cc414a7d94
Leo Dai <42714804+shleodai@users.noreply.github.com>
2023-12-10 21:59:03 -0500

    delete unnecessary files

--- src/findex.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class findex {
-	public static Integer findex;
-	public static List<Integer>  findexlst= new ArrayList<Integer>();
-	
-}
--- src/hindex.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class hindex {
-	public static Integer hindex;
-    public static List<Integer> hindexlst = new ArrayList<Integer>();
-}
